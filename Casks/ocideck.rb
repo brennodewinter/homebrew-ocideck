87ac85a5dce786be10824583dbdf887cb05c6ade
Brenno de Winter <brenno@dewinter.com>
2026-08-04 21:08:23 +0000

    OciDeck v0.3.1: seed Homebrew cask

---
+++ Casks/ocideck.rb
@@ -0,0 +1,11 @@
+cask "ocideck" do
+  version "0.3.1"
+  sha256 "cc3be17ced09b641f646a524b5a8690ab451bba70583259eb64643a276d1a4e7"
+
+  url "https://pawprint.vigilis.online/LibreKAT/Ocideck/releases/download/v0.3.1/ocideck-macos-0.3.1.zip"
+  name "OciDeck"
+  desc "Open-source presentation app for decks, notes, and collaborative sessions"
+  homepage "https://ocideck.librekat.nl"
+
+  app "OciDeck.app"
+end
