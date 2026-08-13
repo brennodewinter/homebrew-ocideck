b812e3b96bb946caffc58034a65c6a6c703f5aa9
OciDeck release <release@librekat.nl>
2026-08-13 23:33:36 +0000

    OciDeck v0.4.3: update Homebrew cask

--- Casks/ocideck.rb
+++ Casks/ocideck.rb
@@ -1,8 +1,8 @@
 cask "ocideck" do
-  version "0.4.2"
-  sha256 "15fc38235aed9258e40751bb1bfb3654e9a21fd8014b9c53bc20637ffe314eaa"
+  version "0.4.3"
+  sha256 "c6c52112ff6bac9eb1362aadc27af1045fa4feefcfd1bce62e71a9d334f0bab4"
 
-  url "https://pawprint.vigilis.online/LibreKAT/Ocideck/releases/download/v0.4.2/ocideck-macos-0.4.2.zip"
+  url "https://pawprint.vigilis.online/LibreKAT/Ocideck/releases/download/v0.4.3/ocideck-macos-0.4.3.zip"
   name "OciDeck"
   desc "Open-source presentation app for decks, notes, and collaborative sessions"
   homepage "https://ocideck.librekat.nl"
